19500b9bfaecf1e74f5a921a10c730e50c2bfaed
Viacheslav-Bo <zotikivna@gmail.com>
2025-12-10 21:14:21 +0200

    aaa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+// src/main.tsx
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+// Нормалізація стилів
+import "modern-normalize";
+// Глобальні стилі (додатково)
+import "./index.css";
+import "./App.css";
+import "./global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
